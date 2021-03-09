12dff895037c6f81fe0c1bebe65d848fe974476c
An Tran <jimmytran411@gmail.com>
2021-03-09 11:41:53 +0200

    fix syntax

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname + "frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/frontend/build/index.html");
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
 app.use("/", products);
