15d66d7ed6517de4c7d34463a886ee913415db34
An Tran <jimmytran411@gmail.com>
2021-04-28 13:42:32 +0300

    add new build

--- frontend/src/utils/LoadingAnimation.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const LoadingAnimation = () => {
-  return (
-    <div className="loading">
-      <b>Loading</b>
-    </div>
-  );
-};
--- frontend/src/utils/config.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-  baseApi: "",
-  // baseApi:
-  //   "https://limitless-retreat-78109.herokuapp.com/https://bad-api-assignment.reaktor.com",
-};
